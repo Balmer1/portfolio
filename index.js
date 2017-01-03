6a9386633006108dc01d382ba124e980f651c63c
Balmer1 <balmer@hotmail.com.ar>
2017-01-03 02:15:59 -0300

    fenotipado page

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var favicon = require('serve-favicon');
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/fenotipado', function (req, res) {
+  res.render('fenotipado.html');
+} );
+
+app.use(favicon(path.join(__dirname,'/views/images/favicon.ico')));
+
+app.listen(process.env.PORT || 3000);
